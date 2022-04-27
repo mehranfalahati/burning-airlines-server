033de133c18cd96f5f94766ba9f3dc22217c0c48
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-28 09:40:38 +1000

    cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+# this configuration was jacked from rack-cors README on github (with see)
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origins '*' # this needs to be an explicit list of URLs on Heroku
+        resource '*',
+        :headers => :any,
+        :methods => %i(get post put patch delete options head)
+    end
+end
